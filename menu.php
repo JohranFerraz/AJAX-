c8008f06ae80d274d81b9bbb86efac2ade5a4293
Johran Ferraz <johran.ferraz@edu.unipar.br>
2022-11-22 22:24:21 -0300

    menu

---
+++ menu.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastro de PC</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css"  href="estilo.css" />
+</head>
+<body>
+
+<nav id="menu">
+	<ul>
+		<li><a href="formInsert.html">formInsert</a></li>
+		<li><a href="menu.php">menu</a></li>
+		<li><a href="login.php">login</a></li>
+		<li><a href="pagina.php">pagina</a></li>
+	</ul>
+</nav><br><br>
+
+<header style="background-color: #c9c9c9">
+	<nav><a href="inserir.php" style="float: left; margin-right: 20px">Inserir</a></nav>
+	<nav><a href="leitura.php">Consultar</a></nav>
+</header>
+</body>
+</html>
